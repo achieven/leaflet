2c0cd126f45d61bf722c54dcb93ca4e4d5a2491c
achieven <achievendar@gmail.com>
2016-11-02 20:23:13 +0200

    added delete trip warning, added validation for name and date

--- app/tripTable.ts
+++ app/tripTable.ts
@@ -17,7 +17,9 @@ export class AppComponent {
         var tripsAsArray = JSON.parse(window.localStorage.getItem('trips'))
         var tripsInTable = ''
         tripsAsArray.forEach(function (trip) {
-            var tableRow = '<tr><td>' + trip.date + '</td> <td>' + trip.name + '</td> <td>' + '<button class="btn btn-info editTrip" data-toggle="modal" data-target="#newTripModal" id=' + trip.id + '>Edit</button><button class="btn btn-info deleteTrip" id=' + trip.id + '>Delete</button></td></tr>'
+            var deleteWarningText = '<td class="deleteTripWarning hide" deleteId=' + trip.id + '><div class="alert alert-warning text-center" id="deleteWarningText">Are you sure you want to delete this trip? This is an irreversible step! </div></td>'
+            var deleteWarningButtons = '<td class="deleteTripWarningButtons hide" deleteId=' + trip.id +'> <button class="btn btn-danger yesDeleteTrip yesButton" id=' + trip.id +' type="button">Yes </button> <button class="btn btn-info noDontDeleteTrip noButton" id=' + trip.id +' type="button">No </button></td>'
+            var tableRow = '<tr><td>' + trip.date + '</td> <td>' + trip.name + '</td> <td class="editAndDeleteTrip">' + '<button class="btn btn-info editTrip" data-toggle="modal" data-target="#newTripModal" id=' + trip.id + '>Edit</button><button class="btn btn-warning deleteTrip" id=' + trip.id + '>Delete</button></td>' + deleteWarningText + deleteWarningButtons +'</tr>'
             tripsInTable += tableRow
         })
         $('.tripTable').html(tripsInTable)
@@ -119,23 +121,27 @@ export class AppComponent {
         $('.saveTrip').unbind('click').on('click', function (e) {
             e.preventDefault()
             var name = $('.createName').val()
-            var date = $('.createDate').val()
-            var id = thisTrip.id
-            var newTripContents = {name: name, date: date, id: id, landmarks: thisTrip.landmarks}
-            var allTrips = JSON.parse(window.localStorage.getItem('trips'))
-            if (newTripOrEdit === 'new') {
-                allTrips.push(newTripContents)
+            var dateToReverse = $('.createDate').val()
+            var date = dateToReverse.split('-').reverse().join('/')
+            
+            if(thisView.validateTripProperties(name, date)){
+                var id = thisTrip.id
+                var newTripContents = {name: name, date: date, id: id, landmarks: thisTrip.landmarks}
+                var allTrips = JSON.parse(window.localStorage.getItem('trips'))
+                if (newTripOrEdit === 'new') {
+                    allTrips.push(newTripContents)
+                }
+                else if (newTripOrEdit === 'edit') {
+                    var tripIndex = allTrips.findIndex(function (_trip) {
+                        return _trip.id === thisTrip.id
+                    })
+                    allTrips[tripIndex] = newTripContents
+                }
+                $('.error-create-trip').addClass('hide')
+                window.localStorage.setItem('trips', JSON.stringify(allTrips))
+                thisView.renderTripsFromLocalStorage();
+                (<any>$('#newTripModal')).modal('hide')
             }
-            else if (newTripOrEdit === 'edit') {
-                var tripIndex = allTrips.findIndex(function (_trip) {
-                    return _trip.id === thisTrip.id
-                })
-                allTrips[tripIndex] = newTripContents
-            }
-
-            window.localStorage.setItem('trips', JSON.stringify(allTrips))
-            thisView.renderTripsFromLocalStorage();
-            (<any>$('#newTripModal')).modal('hide')
         })
     }
 
@@ -173,11 +179,37 @@ export class AppComponent {
 
         })
         $('.deleteTrip').unbind('click').on('click', function (e) {
+            var deleteWarningSelector = '[deleteId=' + JSON.stringify(e.currentTarget.id) + ']'
+            $(deleteWarningSelector).removeClass('hide')
+        })
+        $('.yesDeleteTrip').unbind('click').on('click', function (e) {
             thisView.deleteTripFromLocalStorage(e.currentTarget.id)
             thisView.renderTripsFromLocalStorage()
+            var deleteWarningSelector = '[deleteId=' + JSON.stringify(e.currentTarget.id) + ']'
+            $('.deleteWarningSelector').addClass('hide')
         })
+        $('.noDontDeleteTrip').unbind('click').on('click', function (e) {
+            var deleteWarningSelector = '[deleteId=' + JSON.stringify(e.currentTarget.id) + ']'
+            $(deleteWarningSelector).addClass('hide')
+        })
+    }
 
+    validateTripProperties(name, date){
+        var dateRegex = /^(((0[1-9]|[12]\d|3[01])\/(0[13578]|1[02])\/((19|[2-9]\d)\d{2}))|((0[1-9]|[12]\d|30)\/(0[13456789]|1[012])\/((19|[2-9]\d)\d{2}))|((0[1-9]|1\d|2[0-8])\/02\/((19|[2-9]\d)\d{2}))|(29\/02\/((1[6-9]|[2-9]\d)(0[48]|[2468][048]|[13579][26])|((16|[2468][048]|[3579][26])00))))$/g;
+        var dateValid = dateRegex.test(date)
 
+        if(!dateValid){
+            $('.error-create-trip').removeClass('hide')
+            $('.error-create-trip').text('date is not valid!')
+        }
+        else if(!name){
+            $('.error-create-trip').removeClass('hide')
+            $('.error-create-trip').text('must fill name!')
+        }
+        else {
+            return true
+        }
+        return false
     }
 
 }
